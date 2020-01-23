7c0129f2535f2c105647788727159f37703017b2
Sven Wick <sven.wick@gmx.de>
2020-01-24 00:26:21 +0100

    Update tap for ssh-tools 1.6

--- Formula/ssh-tools.rb
+++ Formula/ssh-tools.rb
@@ -2,8 +2,8 @@
 class SshTools < Formula
   desc "ssh-tools"
   homepage "https://github.com/vaporup/ssh-tools"
-  url "https://github.com/vaporup/ssh-tools/archive/v1.5.tar.gz"
-  sha256 "001e2c796c00236a7f4c5dd29b64eb5cdcf550f71f7017d34756ad41c02204bc"
+  url "https://github.com/vaporup/ssh-tools/archive/v1.6.tar.gz"
+  sha256 "5f6618894108a3f0b240ae908c3465430456e99fe1048cc0a589f1b8cfbbf6e4"
 
   #depends_on "openssh"
   #depends_on "bash"
